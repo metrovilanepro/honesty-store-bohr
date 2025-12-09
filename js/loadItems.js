cf35317b22a5ee5ecca444b500cdb26f8f3d6133
D4RK <adrianfogartybrent@gmail.com>
2025-12-09 22:07:39 +0800

    Added item loading and items.

---
+++ js/loadItems.js
@@ -0,0 +1,42 @@
+const snacks = document.querySelector("#snacks");
+const candy = document.querySelector("#candy");
+const drinks = document.querySelector("#drinks");
+const stationery = document.querySelector("#stationery");
+const crafts = document.querySelector("#crafts");
+
+async function loadJsonFromServer(filePath) {
+    try {
+        const response = await fetch(filePath);
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        return await response.json();
+    } catch (error) {
+        console.error("Error loading JSON:", error);
+        return null;
+    }
+}
+
+loadJsonFromServer('../assets/json/items.json').then(items => {
+    if (!items) return;
+
+
+    Object.keys(items).forEach(category => {
+        const categoryItems = items[category];
+        const container = document.querySelector(`#${category}`);
+
+        if (!container) return;
+
+        categoryItems.forEach(item => {
+            const div = document.createElement('div');
+            div.classList.add("item");
+            div.innerHTML = `
+                <img src="${item.image}">
+                <div>
+                    <h1>${item.name}</h1>
+                    <h2>Price: ${item.price}</h2>
+                </div>
+            `
+
+            container.appendChild(div);
+        });
+    });
+});
