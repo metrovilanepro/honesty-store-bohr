6785bdfe2332adab945e0853a48b824d35ffc6e7
D4RK <adrianfogartybrent@gmail.com>
2025-12-11 05:31:26 +0800

    Finished menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 const navItems = document.querySelectorAll("header nav li");
 const sections = document.querySelectorAll("section");
+const catnavbutton = document.getElementById("catnav2button");
+const catnav = document.getElementById("catnav2");
+const catnavback = document.getElementById("catnav2back");
+const itemCategories = document.querySelectorAll(".item-containers");
+const itemItems = document.querySelectorAll("#menu nav ul li");
+const itemItems2 = document.querySelectorAll("#catnav2 ul li");
 
 function showSection(id) {
     sections.forEach(sec => {
         if (sec.id === id) {
             sec.classList.add("active");
+            if (id === "menu") {
+                catnavbutton.classList.remove("hide-catnav2");
+                catnav.classList.remove("hide-catnav2");
+                showCategory("snacks");
+            } else {
+                catnavbutton.classList.add("hide-catnav2");
+                catnav.classList.add("hide-catnav2");
+            }
         } else {
             sec.classList.remove("active");
         }
@@ -22,11 +36,6 @@ navItems.forEach(item => {
         showSection(page);
     });
 });
-
-
-const itemCategories = document.querySelectorAll(".item-containers");
-const itemItems = document.querySelectorAll("#menu nav ul li");
-
 
 function showCategory(id) {
     itemCategories.forEach(cat => {
@@ -46,4 +55,24 @@ itemItems.forEach(item => {
         const itemCategory = item.getAttribute("data-category");
         showCategory(itemCategory);
     });
+});
+
+itemItems2.forEach(item => {
+    item.addEventListener('click', () => {
+        itemItems2.forEach(i => i.classList.remove("itemactivated2"));
+        item.classList.add("itemactivated2");
+
+        const itemCategory = item.getAttribute("data-category");
+        showCategory(itemCategory);
+    });
+});
+
+catnavbutton.addEventListener('click', () => {
+    catnav.style.right = "0px";
+    console.log("Clicked!");
+});
+
+catnavback.addEventListener('click', () => {
+    catnav.style.right = "-400px";
+    console.log("Clicked!");
 });
